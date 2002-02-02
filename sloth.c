e4d37986cde51a98f9398706db27a89b8ec9ceab
nick <>
2002-02-02 07:19:29 +0000

    typo in comments

--- sloth.c
+++ sloth.c
@@ -1,23 +1,23 @@
 /*
  *  sloth v1.0.0
  *  Copyright (C) 2002  Nick Jennings
- #
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; version 2 of the License. 
- #
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- #
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- #
+ *
  *  This program is developed and maintained by Nick Jennings.
  * Contact Information:
- #
+ *
  *    Nick Jennings                 nick@namodn.com
  *    PMB 338                       http://nick.namodn.com
  *    4096 Piedmont Ave.
